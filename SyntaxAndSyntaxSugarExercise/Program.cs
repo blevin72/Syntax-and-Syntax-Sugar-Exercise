61c5fb1b9f95dede74ed3aff51f861380ff4dc17
Bryan Levin <bryanlevin72@gmail.com>
2023-06-22 11:30:08 -0700

    Exercise 1

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //inferred typing
+            //string interpolation
+            //and the ternary operator
+
+            var answer = 4;
+
+            var response = (answer < 9) ? $"{answer} is less than nine." : $"{answer} is greater than or equal to nine.";
+            Console.WriteLine(response);
+
         }
     }
 }
